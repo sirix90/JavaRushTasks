470707227605d886b305a98c312b308f728d604e
sirix90 <sirix90@mail.ru>
2023-02-13 23:06:42 +0300

    add solution JavaSyntax task 0103

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0103/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task01.task0103;
+
+/*
+Мой юный друг
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int c = 3118;
+        System.out.println(c)       ;
+    }
+}
