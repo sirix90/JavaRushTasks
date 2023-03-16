18c1a7761fefa5dc76cc3fecdc7e5ffcfc4f7243
sirix90 <sirix90@mail.ru>
2023-03-16 23:34:41 +0300

    add solution JavaSyntax task 0511

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0511/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.pro.task05.task0511;
+
+import java.util.Scanner;
+
+/* 
+Создаем двумерный массив
+*/
+
+public class Solution {
+    public static int[][] multiArray;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        multiArray = new int[N][];
+        for (int i = 0; i < N; i++) {
+            int M = scanner.nextInt();
+            multiArray[i] = new int[M];
+        }
+        for (int i = 0; i < multiArray.length; i++) {
+            for (int j = 0; j < multiArray[i].length; j++) {
+                System.out.print(multiArray[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
