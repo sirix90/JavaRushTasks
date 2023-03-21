9e2d5d55e47321110b5995b05effe3ddf61bc761
sirix90 <sirix90@mail.ru>
2023-03-21 22:36:22 +0300

    add solution JavaSyntax task 0606

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,53 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod() {
+
+    }
+
+    public static void universalMethod(String str, int i) {
+
+    }
+
+    public static void universalMethod(int i) {
+
+    }
+
+    public static void universalMethod(String name) {
+
+    }
+
+    public static void universalMethod(int i, String text) {
+
+    }
+
+    public static void universalMethod(String name1, String name2) {
+
+    }
+
+    public static void universalMethod(int i, int j) {
+
+    }
+
+    public static void universalMethod(String str, String name, String name2) {
+
+    }
+
+    public static void universalMethod(String name1, String name2, int age) {
+
+    }
+
+    public static void universalMethod(int age, String text, int hi){
+
+    }
+
+}
