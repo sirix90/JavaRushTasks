9d9b193c4aaf7387ed177a6f56573a9f7542e8ce
sirix90 <sirix90@mail.ru>
2023-03-24 00:22:15 +0300

    add solution JavaSyntax task 0701

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    int i = 10;
+    double j = 6.0;
+    boolean k = true;
+    char l = 1;
+    byte m = 65;
+    short n = 65;
+    long b = 65L;
+    float v = 65F;
+
+}
