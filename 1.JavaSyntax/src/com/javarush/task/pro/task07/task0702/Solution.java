a017e2911689dcbe0ce2783bb58bd6f8ea580cd0
sirix90 <sirix90@mail.ru>
2023-03-26 11:07:28 +0300

    add solution JavaSyntax task 0702

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0702/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task07.task0702;
+
+/* 
+Максимальное целое
+*/
+
+public class Solution {
+   int i =  Integer.MAX_VALUE;
+   long j =  Long.MAX_VALUE;
+    short k = Short.MAX_VALUE;
+    byte l = Byte.MAX_VALUE;
+
+
+}
