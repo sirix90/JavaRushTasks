c0f5b07a4350d292d4d4b22bdea9863f616da6d3
sirix90 <sirix90@mail.ru>
2023-03-26 22:04:03 +0300

    add solution JavaSyntax task 0712

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String i = new String("ghh");
+        String i1 = new String("ghh");
+        String i2 = new String("ghh");
+        String i3 = new String("ghh");
+        String i4 = new String("ghh");
+        String i5 = new String("ghh");
+        String i6 = new String("ghh");
+        String i7 = new String("ghh");
+        String i8 = new String("ghh");
+        String i9 = new String("ghh");
+        int[] j = new int[6];
+        int[] j1 = new int[6];
+        int[] j2 = new int[6];
+        int[] j3 = new int[6];
+        int[] j4 = new int[6];
+        Scanner scanner = new Scanner("");
+        Scanner scanner1 = new Scanner("");
+
+    }
+}
