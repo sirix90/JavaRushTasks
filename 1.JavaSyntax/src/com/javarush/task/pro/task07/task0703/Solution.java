6c41032c43c75b78090a6a5b3f79675ccd7976ac
sirix90 <sirix90@mail.ru>
2023-03-26 11:10:08 +0300

    add solution JavaSyntax task 0703

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0703/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task07.task0703;
+
+/* 
+Минимальное целое
+*/
+
+public class Solution {
+    byte i = Byte.MIN_VALUE;
+    short j = Short.MIN_VALUE;
+    int k = Integer.MIN_VALUE;
+    long l = Long.MIN_VALUE;
+}
